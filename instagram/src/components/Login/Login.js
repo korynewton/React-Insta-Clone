7a96ad80384aff3249c6be727eba8f159f399508
korynewton <korynewtondev@gmail.com>
2019-02-20 13:41:28 -0800

    styled login page with reactstrap

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import {
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
+  } from 'reactstrap';
 
 
 
@@ -24,9 +29,34 @@ class LoginPage extends React.Component {
     render() {
         return(
             <>
-                <input type="text" placeholder="username" name="userText" value={this.state.userText} onChange={this.handleOnChange}/>
-                <input type="password" name="" placeholder="password" name="passText" value={this.state.passText} onChange={this.handleOnChange}/>
-                <button onClick={this.login}>Login</button>
+                 <Container className="App">
+                    <h2>Sign In</h2>
+                    <Form className="form" onSubmit={this.login}>
+                    <Col>
+                        <FormGroup>
+                        <Label>Username</Label>
+                        <Input
+                         placeholder='username'
+                         name="userText"
+                         value={this.state.userText} 
+                         onChange={this.handleOnChange}
+                         />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup>
+                        <Label for="examplePassword">Password</Label>
+                        <Input 
+                        type="password" 
+                        name="" placeholder="password" 
+                        name="passText" value={this.state.passText} 
+                        onChange={this.handleOnChange}
+                        />
+                        </FormGroup>
+                    </Col>
+                    <Button onSubmit={this.login}>Submit</Button>
+                    </Form>
+                </Container>
             </>
         )
     }
