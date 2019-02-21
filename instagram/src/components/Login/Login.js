9aaaac29ab13781fa9e911520d9a7a8868df9e2a
korynewton <korynewtondev@gmail.com>
2019-02-21 14:41:01 -0800

    login page converted to styled-components from reactstrap

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,9 +1,58 @@
 import React from 'react';
-import {
-    Container, Col, Form,
-    FormGroup, Label, Input,
-    Button,
-  } from 'reactstrap';
+import styled from 'styled-components';
+import instaLogo from '../../images/icons/instagramLogo.svg'
+
+
+
+const MainContainer = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    margin: 40px auto;
+    width: 300px;
+    background: lightgray;
+    font-family: 'Segoe UI';
+`
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-flow: column nowrap;
+    /* border: 1px solid red; */
+    text-align: center;
+    margin: 10px 0;
+
+    & h2 {
+        font-size: 30px;
+
+    }
+    
+`
+
+const StyledInput = styled.input`
+    width: 150px;
+    height: 20px;
+    margin: 10px 0;
+`
+const StyledButton = styled.button`
+    padding: 6px 10px;
+    margin: 5px;
+    border: none;
+    border-radius: 3px;
+    color: white;
+    background: #4caf50;
+
+    &:hover {
+        cursor:pointer;
+    }
+`
+
+const StyledLogo = styled.img`
+    display: block;
+    max-width: 300px;
+    margin: auto;
+`
 
 
 
@@ -33,34 +82,27 @@ class LoginPage extends React.Component {
     render() {
         return(
             <>
-                 <Container className="App">
-                    <h2>Sign In</h2>
-                    <Form className="form" onSubmit={this.login}>
-                    <Col>
-                        <FormGroup>
-                        <Label>Username</Label>
-                        <Input
+
+                <StyledLogo src={instaLogo} alt="Instagram"/>
+                 <MainContainer className="App">
+                    <StyledForm onSubmit={this.login}>
+                        <h2>Sign In:</h2>
+                        <StyledInput
                          placeholder='username'
                          name="userText"
                          value={this.state.userText} 
                          onChange={this.handleOnChange}
-                         />
-                        </FormGroup>
-                    </Col>
-                    <Col>
-                        <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input 
+                         />                  
+                   
+                        <StyledInput 
                         type="password" 
                         placeholder="password" 
                         name="passText" value={this.state.passText} 
                         onChange={this.handleOnChange}
-                        />
-                        </FormGroup>
-                    </Col>
-                    <Button onSubmit={this.login}>Submit</Button>
-                    </Form>
-                </Container>
+                        />        
+                        <StyledButton onSubmit={this.login}>Submit</StyledButton>
+                    </StyledForm>
+                </MainContainer>
             </>
         )
     }
