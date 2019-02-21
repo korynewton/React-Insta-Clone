e5ae136e4346c778c7fbd080c505359b4a87dbe7
korynewton <korynewtondev@gmail.com>
2019-02-21 11:47:34 -0800

    header refactored with styled-components

--- instagram/src/components/Header/Header.js
+++ instagram/src/components/Header/Header.js
@@ -5,26 +5,56 @@ import navigate from '../../images/icons/compass.svg';
 import like from '../../images/icons/heart.svg';
 import user from '../../images/icons/user.svg';
 import logo from '../../images/icons/instagramLogo.svg';
-import './Header.css';
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+    /* border: 1px solid black; */
+`
+const LogoHeader = styled.div`
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 1px solid red; */
+`
+const LogoImage = styled.img`
+    max-height: 30px;
+    width:auto;
+`
+
+const SearchDiv = styled.div`
+    width: 30%;
+    /* border: 1px solid purple; */
+`
+const NavIconsDiv = styled.div`
+    width: 30%;
+    display: flex;
+    justify-content: space-around;
+`
+
 
 
 const Header = (props) => {
     return (
-        <div className='header'>
-            <div className="headerLeft headerBox">
-                <img src={instaLogo} alt="Instagram"/>
+        <StyledHeader>
+            <LogoHeader>
+                <LogoImage src={instaLogo} alt="Instagram"/>
                 <h3> | </h3>
-                <img src={logo} alt="Instagram"/>
-            </div>
-            <div className='headerCenter headerBox'>
+                <LogoImage src={logo} alt="Instagram"/>
+            </LogoHeader>
+            <SearchDiv>
                 <SearchBar onSearch={props.onSearch} />
-            </div>
-            <div className="headerRight headerBox">
+            </SearchDiv>
+            <NavIconsDiv>
                 <img src={navigate} alt="Navigation"/>
                 <img src={like} alt="Like"/>
                 <img src={user} alt="Profile"/>
-            </div>
-        </div>
+            </NavIconsDiv>
+        </StyledHeader>
     )
 }
 
